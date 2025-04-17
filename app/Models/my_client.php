789ce13fd530bec81f692d72e89110ef1f4e8f8d
pratomosidiq123 <pratomosidiq123@gmail.com>
2025-04-17 09:42:06 +0700

    fix model my client

--- app/Models/my_client.php
+++ app/Models/my_client.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class my_client extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'my_client';
+    
+    protected $fillable = [
+        'name', 'slug', 'is_project', 'self_capture', 
+        'client_prefix', 'client_logo', 'address', 
+        'phone_number', 'city'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
 }
